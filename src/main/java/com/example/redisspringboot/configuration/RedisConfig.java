0d13669220681e3cac2deda01b4423ede06166cd
77kkyu <77kkyu@gmail.com>
2022-01-09 00:05:42 +0900

    feat RedisConfig

---
+++ src/main/java/com/example/redisspringboot/configuration/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.example.redisspringboot.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        return new LettuceConnectionFactory("localhost", 6379);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory());
+        return redisTemplate;
+    }
+
+}
